5f270744483e2ff7ae2159a6ed97d776e5ea8b89
rmbicfcom <richard.ballew@icf.com>
2021-05-20 01:17:09 -0400

    updated to deal with variant values for identifying diagnostic testing

--- SSICAL/gethtml.py
+++ SSICAL/gethtml.py
@@ -9,7 +9,29 @@ import requests
 import re
 import sys
 
-url = "https://secure.ssa.gov/apps10/poms.nsf/lnx/0423022375"
+def retmatch(st, lst):
+    retval = "None"
+    for le in lst:
+        if st.split(" ")[0] == le.split(" ")[0]:
+            retval = le
+    return retval
+
+#url = "https://secure.ssa.gov/apps10/poms.nsf/lnx/0423022375"
+# the following link had issues
+url = "https://secure.ssa.gov/poms.nsf/lnx/0423022927"
+"""
+https://secure.ssa.gov/poms.nsf/lnx/0423022365
+https://secure.ssa.gov/poms.nsf/lnx/0423022130
+https://secure.ssa.gov/poms.nsf/lnx/0423022150
+https://secure.ssa.gov/poms.nsf/lnx/0423022155
+https://secure.ssa.gov/poms.nsf/lnx/0423022185
+https://secure.ssa.gov/poms.nsf/lnx/0423022440
+https://secure.ssa.gov/poms.nsf/lnx/0423022973
+https://secure.ssa.gov/poms.nsf/lnx/0423022270
+https://secure.ssa.gov/poms.nsf/lnx/0423022645
+https://secure.ssa.gov/poms.nsf/lnx/0423022290
+https://secure.ssa.gov/poms.nsf/lnx/0423022347
+"""
 # define columns of interest. some of these will match the first data element in a table row
 cols = ['SECTION_NUMBER',
         'ICD-9',
@@ -35,12 +57,13 @@ soup = BeautifulSoup(html_content.text, "html.parser") # "lxml")
 
 # find the table
 caltable = soup.find("table", attrs={"class": "poms-table poms-table-brdr-all", "id": "tbl_1"})
-print(caltable.prettify())
+#print(re.sub(u"\u200b", " ", caltable.prettify()))
 #sys.exit()
 # find all rows
 calrows = caltable.tbody.find_all("tr")
 i = 0
 d = {}
+#print("***** ROW DATA *****")
 for row in calrows:
     rowdata = []
     for td in row.find_all("td"):
@@ -52,14 +75,19 @@ for row in calrows:
             print("tried and failed")
             pass
     # print row data
-    #print(str(i) + "\t" + "\t".join(rowdata))
+    print(re.sub(u"\u200b", " ", str(i) + "\t" + "\t".join(rowdata)))
     # purge extra spaces from row headings
     rowdata[0] = re.sub(" +", " ", rowdata[0])
-    if(rowdata[0] in cols):
-        #print(rowdata[0])
-        d[rowdata[0]] = rowdata[1]
+    rowdata[0] = re.sub("DESCRIPTION DIAGNOSTIC", "DIAGNOSTIC", rowdata[0])
+    rowdata[0] = re.sub("DIAGNOSTICTESTING", "DIAGNOSTIC TESTING", rowdata[0])
+    #print("ROWDATA[0]\t", rowdata[0])
+    # if(rowdata[0] in cols):
+    #     #print(rowdata[0])
+    # return the matched value as a valid key in cols
+    key = retmatch(rowdata[0], cols)
+    if key != "None":
+        d[key] = rowdata[1]
     i += 1
-#sys.exit()
 
 # grab the hidden tag data
 # disease name
